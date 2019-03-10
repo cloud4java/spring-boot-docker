6742f74d017eabc849bc772a7d2d85f407c01aff
Dorival <dorival.querino@gmail.com>
2019-03-09 22:20:17 -0300

    Added javadoc.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
     @RequestMapping("/")
+    /*
+    * Method to return Hello Docker World
+     */
     public String home() {
         return "Hello Docker World";
     }
